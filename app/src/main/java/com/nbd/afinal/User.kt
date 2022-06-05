a3375955c667fc4565015569b7aed451e54a21ec
IngOrdaz <91166458+IngOrdaz@users.noreply.github.com>
2022-06-04 20:46:38 -0500

    modelo user creado

---
+++ app/src/main/java/com/nbd/afinal/User.kt
@@ -0,0 +1,9 @@
+package com.nbd.afinal
+
+class User (name:String,username:String, level:Int,points:Int, nextLevelPoints:Int){
+    val name=name
+    val username= username
+    val level=level
+    val points=points
+    val nextLevelPoints=nextLevelPoints
+}
