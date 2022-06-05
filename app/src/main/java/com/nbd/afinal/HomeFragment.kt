a5cf65797b4b48085531c3b7893d0f5958f67f64
IngOrdaz <91166458+IngOrdaz@users.noreply.github.com>
2022-06-04 20:18:56 -0500

    funciona navbar, perfil, inicio

---
+++ app/src/main/java/com/nbd/afinal/HomeFragment.kt
@@ -0,0 +1,26 @@
+package com.nbd.afinal
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.nbd.afinal.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding=FragmentHomeBinding.inflate(layoutInflater)
+
+        val navConroller=findNavController()
+        val destination=HomeFragmentDirections.actionHomeFragmentToProfileFragment( )
+        return binding.root
+    }
+
+}
